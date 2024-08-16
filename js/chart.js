90ea94b4afb64111c357c09e41d700aca9562395
ParkGana <gnchoco97@gmail.com>
2024-08-16 12:01:50 +0900

    LINE CHART 구현

--- js/chart.js
+++ js/chart.js
@@ -68,4 +68,77 @@ function ChartEvent() {
             }
         }
     })
+
+    /****************************************************************************************************/
+
+    let line = document.getElementById('chart-line')
+
+    new Chart(line, {
+        type: 'line',
+        data: {
+            labels: ['2021', '2022', '2023', '2024'],
+            datasets: [
+                {
+                    label: 'A',
+                    data: [20, 10, 50, 70],
+                    backgroundColor: 'rgba(255, 0, 0, 0.3)',
+                    borderColor: 'rgba(255, 0, 0, 0.3)',
+                    borderWidth: 2
+                },
+                {
+                    label: 'B',
+                    data: [40, 60, 90, 50],
+                    backgroundColor: 'rgba(0, 0, 255, 0.3)',
+                    borderColor: 'rgba(0, 0, 255, 0.3)',
+                    borderWidth: 2
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    border: {
+                        display: false,
+                        color: 'rgba(0, 0, 0, 0.1)',
+                        lineWidth: 1
+                    },
+                    grid: {
+                        display: false,
+                        color: 'rgba(0, 0, 0, 0.1)',
+                        lineWidth: 1
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    max: 100,
+                    ticks: {
+                        display: true,
+                        stepSize: 20
+                    },
+
+                    border: {
+                        display: false,
+                        color: 'rgba(0, 0, 0, 0.1)',
+                        lineWidth: 1
+                    },
+                    grid: {
+                        display: true,
+                        color: 'rgba(0, 0, 0, 0.1)',
+                        lineWidth: 1
+                    }
+                }
+            },
+            plugins: {
+                title: {
+                    display: false,
+                    text: 'Line Chart Title'
+                },
+                legend: {
+                    display: false,
+                    position: 'top'
+                }
+            }
+        }
+    })
 }
